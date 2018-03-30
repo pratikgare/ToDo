13f3cc79bc8d67e4b129cb7032d90bc6a235c0f5
pratik gare <pratik.gare69@gmail.com>
2018-03-30 03:23:22 -0700

    understanding component.ts

--- MyToDoApp/src/app/app.component.ts
+++ MyToDoApp/src/app/app.component.ts
@@ -1,10 +1,14 @@
+// imports
 import { Component } from '@angular/core';
 
+// component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// logic for angular 5
 export class AppComponent {
   title = 'app';
 }
